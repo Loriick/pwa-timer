0d246405658f2881bdd956d6e8bcaf7b0a19111f
Travailleur Lorick <loricktravailleur@gmail.com>
2018-12-26 23:16:56 +0100

    add first countdown and display to the screen

--- src/components/timerDisplay/TimerDisplay.js
+++ src/components/timerDisplay/TimerDisplay.js
@@ -4,43 +4,59 @@ import MyContext from "../../Context";
 export default class TimerDisplay extends Component {
   static contextType = MyContext;
   state = {
-    timer: { ...this.context.timer }
+    timer: { ...this.context.timer },
+    isLoop: true
   };
 
   componentDidMount() {
-    this.timerFunc(this.state.timer.workout);
+    this.countDownTime(
+      "workout",
+      this.state.timer.workout,
+      "sets",
+      this.state.timer.sets
+    );
   }
 
-  timerFunc = seconds => {
-    const now = Date.now();
-    const then = now + seconds * 1000;
+  countDownTime = (categoryTime, time, categoryRep, rep) => {
+    const timer = { ...this.state.timer };
+    const second = timer[categoryTime];
 
-    this.countdown = setInterval(() => {
-      const secondsLeft = Math.round((then - Date.now()) / 1000);
-      //stop
+    this.countDown = setInterval(() => {
+      if (rep > 0) {
+        timer[categoryTime] = time -= 1;
+        this.setState({ timer });
 
-      /**
-       *
-       * maintenant quand timer arrive a 0 on verife si il y a un cycle on fait partir alors
-       * le rest timer quand le rest time est a zero on verifie s'il reste des cycles si il en reste on
-       * fait partir alors on workout et cycle et rep prenne -1
-       */
-      if (secondsLeft < 0) {
-        clearInterval(this.countdown);
-        return;
-      }
-
-      this.setState(prev => ({
-        timer: {
-          workout: secondsLeft
+        if (time <= -1) {
+          time = second;
+          timer[categoryTime] = time;
+          timer[categoryRep] = rep -= 1;
+          this.setState({ timer });
         }
-      }));
-      console.log(secondsLeft);
+      } else {
+        this.stopCountDown(this.countDown);
+      }
     }, 1000);
+  };
+
+  countDownRepition = (category, rep) => {
+    const timer = { ...this.state.timer };
+    timer[category] = rep -= 1;
+    console.log(rep);
+    this.setState({ timer });
+  };
+
+  stopCountDown = timer => {
+    clearInterval(timer);
+    return;
   };
 
   render() {
     console.log(this.state.timer);
-    return <div>{this.state.timer.workout}</div>;
+    return (
+      <>
+        <div> time {this.state.timer.workout}</div>
+        <div>sets {this.state.timer.sets}</div>
+      </>
+    );
   }
 }
