6e94a51e1f1126f254865fd4022ac78c83b9b065
Travailleur Lorick <loricktravailleur@gmail.com>
2018-12-30 17:51:51 +0100

    add github page depencie

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import Settings from "./settings/Settings";
@@ -7,14 +7,14 @@ import TimerDisplay from "./timerDisplay/index";
 import Finish from "./Finish";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/settings" component={Settings} />
       <Route path="/timer" component={TimerDisplay} />
       <Route path="/finish" component={Finish} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
